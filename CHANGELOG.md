86987fb4eae0c5d9929532b5ea74d0bf36877778
florintudoran <61730039+florintudoran@users.noreply.github.com>
2021-09-24 10:24:19 +0200

    Initialize opera

---
+++ CHANGELOG.md
@@ -0,0 +1,20 @@
+# Changelog
+
+## 2.1.0 (Released 09.03.2020)
+ - Added execution on localhost selenium hub.
+ - Added README
+
+## 2.0.0 (Released 08.03.2020)
+ - Added LICENSE for using.
+ - Added Firefox job.
+ - Added --browser option for selecting chrome or firefox to run tests in. Default is chrome.
+ - Renamed fixture 'browser' to 'remote_browser'.
+
+## 1.0.0 (Released 08.03.2020)
+ - Bump version for release.
+
+## 0.1.1
+ - fixed: throw chrome Options() to Remote webdriver
+
+## 0.1.0
+ - created simple gitlab-ci testing project with selenium, remote standalone-chrome and one selenium test in ci job.
---
+++ conftest.py
@@ -0,0 +1,94 @@
+# MIT License
+#
+# Copyright (c) 2020 Aleksandr Kotlyar
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import pytest
+from selenium import webdriver
+from selenium.webdriver import Remote
+
+
+def pytest_addoption(parser):
+    """ Parse pytest --option variables from shell """
+    parser.addoption('--browser',
+                     help='Which test browser?',
+                     default='chrome')
+    parser.addoption('--local',
+                     help='local or CI?',
+                     choices=['true', 'false'],
+                     default='true')
+
+
+@pytest.fixture(scope='session')
+def test_browser(request):
+    return request.config.getoption('--browser')
+
+
+@pytest.fixture(scope='session')
+def local(request):
+    return request.config.getoption('--local')
+
+
+@pytest.fixture(scope='function')
+def remote_browser(test_browser, local) -> Remote:
+    """ Select configuration depends on browser and host
+    """
+    if local == 'false':
+        if test_browser == 'firefox':
+            driver = webdriver.Remote(
+                options=webdriver.FirefoxOptions(),
+                command_executor='http://selenium__standalone-firefox'
+                                 ':4444/wd/hub')
+        elif test_browser == 'chrome':
+            driver = webdriver.Remote(
+                options=webdriver.ChromeOptions(),
+                command_executor='http://selenium__standalone-chrome'
+                                 ':4444/wd/hub')
+        elif test_browser == 'opera':
+            driver = webdriver.Remote(
+                options=webdriver.ChromeOptions(),
+                command_executor='http://selenium__standalone-opera'
+                                 ':4444/wd/hub')
+        else:
+            raise ValueError(f'--browser="{test_browser}" '
+                             f'is not chrome or firefox')
+    elif local == 'true':
+        if test_browser == 'firefox':
+            driver = webdriver.Remote(
+                options=webdriver.FirefoxOptions(),
+                command_executor='http://localhost:4444/wd/hub')
+        elif test_browser == 'chrome':
+            driver = webdriver.Remote(
+                options=webdriver.ChromeOptions(),
+                command_executor='http://localhost:4444/wd/hub')
+        elif test_browser == 'opera':
+            driver = webdriver.Remote(
+                options=webdriver.ChromeOptions(),
+                command_executor='http://localhost:4444/wd/hub')
+        else:
+            raise ValueError(f'--browser="{test_browser}" '
+                             f'is not chrome or firefox')
+    else:
+        raise ValueError(f'--local={local}". Driver could not be setup.\n'
+                         'pass option --local="true" if local execute\n'
+                         'pass option --local="false" if use CI service')
+
+    yield driver
+    driver.quit()
